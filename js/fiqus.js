6d4832980f8097efef5b253eac3b7d55cc8f2646
Mariano Lambir <mlambir@gmail.com>
2012-05-09 23:26:28 -0300

    separe dibujo de generacion

--- js/fiqus.js
+++ js/fiqus.js
@@ -1,6 +1,12 @@
-var directions = [[-1, 1], [-1, 0], [-1,-1], [ 0,-1], [ 1,-1], [ 1, 0], [ 1, 1]];
+var directions = [[0, 1],[-1, 1], [-1, 0], [-1,-1], [ 0,-1], [ 1,-1], [ 1, 0], [ 1, 1], [0, 1]];
 
-var Tree = function(canvas, grid_size, branchWidth, nLines, trunk_len, rad, turning_chances, splitting_chances){
+var Branch = function(point, direction){
+	this.point = point;
+	this.direction = direction;
+	this.children = [];
+};
+
+var Tree = function(canvas, grid_size, branchWidth, nLines, trunk_len, rad, turning_chances, splitting_chances, death_chances){
 	this.set_canvas(canvas);
 	this.grid_size = grid_size;
 	this.branchWidth = branchWidth;
@@ -8,6 +14,7 @@ var Tree = function(canvas, grid_size, branchWidth, nLines, trunk_len, rad, turn
 	this.trunk_len = trunk_len;
 	this.turning_chances = turning_chances;
 	this.splitting_chances = splitting_chances;
+	this.death_chances = death_chances;
 
 	this.grid_total_width = Math.floor(this.width / this.grid_size) * this.grid_size;
 	this.grid_total_height = Math.floor(this.height / this.grid_size) * this.grid_size;
@@ -31,21 +38,6 @@ Tree.prototype.set_canvas = function(c) {
 	this.width = c.width;
 	this.height = c.height;
 };
-
-Tree.prototype.generate = function() {
-	//inicializo la matriz;
-	this.matrix = [];
-	for(var i=0;i<this.cols+1;i++){
-		this.matrix[i] = [];
-	}
-
-	this.drawn_squares = [];
-	for(i=0;i<this.cols;i++){
-		this.drawn_squares[i] = [];
-	}
-};
-
-
 
 Tree.prototype.can_draw = function(point_from, n_direction) {
 	if(n_direction < 0){
@@ -71,7 +63,7 @@ Tree.prototype.can_draw = function(point_from, n_direction) {
 	var dir = directions[n_direction];
 
 	if(Math.abs(dir[0])> 0 && Math.abs(dir[1])> 0){ /*si es diagonal*/
-		if(this.drawn_squares[Math.min(point_from[0], next_point[0])][Math.min(point_from[1], next_point[1])]){
+		if(this.used_squares[Math.min(point_from[0], next_point[0])][Math.min(point_from[1], next_point[1])]){
 			return false;
 		}
 	}
@@ -84,15 +76,15 @@ Tree.prototype.get_checked_direction = function(point_from, n_direction){
 	var possible_directions = [];
 	if(Math.random() < this.turning_chances){
 		if(Math.random() < 0.5){
-			possible_directions = [n_direction+1, n_direction-1, n_direction];
+			possible_directions = [n_direction+1, n_direction-1, n_direction, n_direction+2, n_direction-2];
 		}else{
-			possible_directions = [n_direction-1, n_direction+1, n_direction];
+			possible_directions = [n_direction-1, n_direction+1, n_direction, n_direction-2, n_direction+2];
 		}
 	}else{
 		if(Math.random() < 0.5){
-			possible_directions = [n_direction, n_direction-1, n_direction+1];
+			possible_directions = [n_direction, n_direction-1, n_direction+1, n_direction-2, n_direction+2 ];
 		}else{
-			possible_directions = [n_direction, n_direction+1, n_direction-1];
+			possible_directions = [n_direction, n_direction+1, n_direction-1, n_direction+2, n_direction-2];
 		}
 	}
 	for (var i = 0; i < possible_directions.length; i++) {
@@ -103,77 +95,121 @@ Tree.prototype.get_checked_direction = function(point_from, n_direction){
 	return;
 };
 
-Tree.prototype.draw = function() {
-	var drawing = true;
+Tree.prototype.new_branch = function(parent, direction){
+	dir = directions[direction];
+	branch = new Branch([parent.point[0] + dir[0], parent.point[1] + dir[1]], direction);
+	this.matrix[branch.point[0]][branch.point[1]] = true;
+	if(Math.abs(dir[0])> 0 && Math.abs(dir[1])> 0){ /*si es diagonal*/
+		this.used_squares[Math.min(parent.point[0], branch.point[0])][Math.min(parent.point[1], branch.point[1])] = true;
+	}
+	parent.children.push(branch);
+	return branch;
+};
+
+Tree.prototype.generate = function() {
+	//inicializo la matriz;
+	this.matrix = [];
+	for(var i=0;i<this.cols+1;i++){
+		this.matrix[i] = [];
+	}
+
+	this.used_squares = [];
+	for(i=0;i<this.cols;i++){
+		this.used_squares[i] = [];
+	}
+
+	this.generated_tree = [];
+
+	var active_branches = [];
+	for(i=0;i<this.nLines;i++){
+		var branch = new Branch([this.cols - Math.floor((this.cols+1)/2) - Math.floor(this.nLines/2) + i , this.rows-2 ], 4);
+		active_branches.push(branch);
+		this.generated_tree.push(branch);
+	}
+
 	var n = 0;
+	while (active_branches.length > 0){
+		var next_active_branches = [];
+		var l = active_branches.length;
+		for (i = 0; i < l; i++) {
+			var curr_branch = active_branches[i];
+			var direction = curr_branch.direction;
+			if(n<this.trunk_len){
+				next_active_branches.push(this.new_branch(curr_branch, direction));
+			}
+			if(n == this.trunk_len){ /*termine el tronco, hago que se abran las ramas*/
+				if(i < active_branches.length/3 - 1){
+					next_active_branches.push(this.new_branch(curr_branch, 3));
+				}else if(i > 2 * active_branches.length/3){
+					next_active_branches.push(this.new_branch(curr_branch, 5));
+				}else{
+					next_active_branches.push(this.new_branch(curr_branch, direction));
+				}
+			}else if(n > this.trunk_len){/*las ramas*/
+				if(Math.random() > this.death_chances){
+					direction = this.get_checked_direction(curr_branch.point, curr_branch.direction);
+					if(direction !== undefined){
+						next_active_branches.push(this.new_branch(curr_branch, direction));
+						if(Math.random() < this.splitting_chances){/*nueva rama*/
+							direction = this.get_checked_direction(curr_branch.point, curr_branch.direction);
+							if(direction !== undefined){
+								next_active_branches.push(this.new_branch(curr_branch, direction));
+							}
+						}
+					}
+				}
+			}
+		}
+		active_branches = next_active_branches;
+		n++;
+	}
+	console.debug(this.generated_tree);
+};
+
+Tree.prototype.draw = function() {
+	
+	var current_branches = [];
+	for (var i = 0; i < this.generated_tree.length; i++) {
+		current_branches.push(this.generated_tree[i]);
+	}
 
 	this.ctx.fillStyle = this.back_color;
-	this.ctx.strokeStyle = this.fore_color;
+	this.ctx.lineStyle = this.fore_color;
 
 	this.ctx.fillRect(0,0,this.width,this.height);
 
-	var puntos = [];
-	for(i=0;i<this.nLines;i++){
-		var p = [this.cols - Math.floor((this.cols+1)/2) - Math.floor(this.nLines/2) + i , this.rows-2 ];
-		puntos.push(p);
-		this.matrix[p[0]][p[1]] = 3; //direccion para arriba
-
-	}
-
-	while (drawing){
-		drawing = false;
-		var l = puntos.length;
-		for (i = 0; i < l; i++) {
-			var punto_prev = puntos[i];
-			if(!punto_prev){
-				continue;
-			}
-			var direccion = this.matrix[punto_prev[0]][punto_prev[1]];
-			if(n == this.trunk_len){ /*termine el tronco, hago que se abran las ramas*/
-				if(i < puntos.length/3 - 1){
-					direccion = 2;
-				}else if(i > 2 * puntos.length/3){
-					direccion = 4;
-				}
-			}else if(n > this.trunk_len){/*las ramas*/
-				direccion = this.get_checked_direction(punto_prev, direccion);
-				if(direccion === undefined){
-					puntos[i] = false;
+	var n = 0;
+	while(current_branches.length > 0){
+		next_branches = [];
+		for (i = 0; i < current_branches.length; i++) {
+			branch = current_branches[i];
+			if(branch.children.length>0){
+				for (var j = 0; j < branch.children.length; j++) {
+					child = branch.children[j];
+					
+					next_branches.push(child);
+					
 					this.ctx.beginPath();
-					this.ctx.arc(this.base_x + this.grid_size * punto_prev[0],
-						this.base_y + this.grid_size * punto_prev[1],
-						this.branchWidth, 0, 2*Math.PI);
-					this.ctx.fill();
+					this.ctx.moveTo(this.base_x + this.grid_size * branch.point[0],this.base_y + this.grid_size * branch.point[1]);
+					this.ctx.lineTo(this.base_x + this.grid_size * child.point[0],this.base_y + this.grid_size * child.point[1]);
+					this.ctx.strokeStyle = this.fore_color;
+					if(n < this.trunk_len/3){
+						this.ctx.lineWidth = this.branchWidth * 2;
+					}else{
+						this.ctx.lineWidth = this.branchWidth;
+					}
 					this.ctx.stroke();
-					continue;
-				}if(Math.random() < this.splitting_chances){/*nueva rama*/
-					puntos.push(punto_prev);
 				}
-			}
-
-			var dir = directions[direccion];
-			var punto_next = [punto_prev[0] + dir[0], punto_prev[1] + dir[1]];
-
-			this.matrix[punto_next[0]][punto_next[1]] = direccion;
-			puntos[i] = punto_next;
-
-			this.ctx.beginPath();
-			this.ctx.moveTo(this.base_x + this.grid_size * punto_prev[0],this.base_y + this.grid_size * punto_prev[1]);
-			this.ctx.lineTo(this.base_x + this.grid_size * punto_next[0],this.base_y + this.grid_size * punto_next[1]);
-			this.ctx.strokeStyle = this.fore_color;
-			if(n < this.trunk_len/3){
-				this.ctx.lineWidth = this.branchWidth * 2;
 			}else{
-				this.ctx.lineWidth = this.branchWidth;
+				this.ctx.beginPath();
+				this.ctx.arc(this.base_x + this.grid_size * branch.point[0],
+								this.base_y + this.grid_size * branch.point[1],
+								this.branchWidth, 0, 2*Math.PI);
+				this.ctx.fill();
+				this.ctx.stroke();
 			}
-			this.ctx.stroke();
-
-			if(Math.abs(dir[0])> 0 && Math.abs(dir[1])> 0){ /*si es diagonal*/
-				this.drawn_squares[Math.min(punto_prev[0], punto_next[0])][Math.min(punto_prev[1], punto_next[1])] = true;
-			}
-
-			drawing = true;
 		}
+		current_branches = next_branches;
 		n++;
 	}
 };
